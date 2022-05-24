66ac01a8fdf550f057023a3586bf06bc641b7275
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-05-24 01:03:31 -0700

    added more methods and classes for each category

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-async function getCategories(){
+
+
+async function getCategories() {
     let response = await fetch("https://jservice.io/api/categories?count=4&offset=4")
     let data = await response.json()
     return data
+    
 }
 
 function getOneCategory(category){
-    return `<div class="category"> </div>`
+    return `<div class="category"> ${category.title} </div>
+            <div class="player-clue-100"> $100 </div>
+            <div class="player-clue-200"> $200 </div>
+            <div class="player-clue-300"> $300 </div>
+            <div class="player-clue-400"> $400 </div>
+    `
 }
 
 getCategories().then(categories => {
-
+    console.log(categories)
+    document.body.innerHTML = `<div class="categories">
+        ${categories.map(getOneCategory).join('')}
+    </div>`
 })
